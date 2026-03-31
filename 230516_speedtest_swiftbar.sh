b6b14f4977f87bb6b3dc88f485306b9506594177
hnsol <masatora.bd5@gmail.com>
2026-03-31 20:40:43 +0900

    fix: improve SSID detection and parsing for Wi-Fi logs

--- 230516_speedtest_swiftbar.sh
+++ 230516_speedtest_swiftbar.sh
@@ -15,8 +15,55 @@ current_month=$(date '+%Y-%m')
 logfile="/Users/masatora/Documents/MyDevelop/230100/230516_SwiftBarPlugin/230516_speedtest_swiftbar_${current_month}.log"
 icon=":wifi.square.fill: | sfsize=16"
 
+get_current_ssid() {
+  local wifi_device=""
+  local ssid=""
+
+  wifi_device=$(networksetup -listallhardwareports 2>/dev/null | awk '
+    /Hardware Port: (Wi-Fi|AirPort)/ {
+      getline
+      if ($1 == "Device:") {
+        print $2
+        exit
+      }
+    }
+  ')
+
+  if [ -n "$wifi_device" ]; then
+    ssid=$(networksetup -getairportnetwork "$wifi_device" 2>/dev/null | sed -E 's/^Current (Wi-Fi|AirPort) Network: //')
+
+    case "$ssid" in
+      ""|"You are not associated with an AirPort network."*)
+        ssid=""
+        ;;
+    esac
+  fi
+
+  if [ -z "$ssid" ]; then
+    ssid=$(system_profiler SPAirPortDataType 2>/dev/null | awk '
+      /Current Network Information:/ {
+        getline
+        gsub(/^ +|:$/, "")
+        print
+        exit
+      }
+      /^ *SSID: / {
+        sub(/^ *SSID: /, "")
+        print
+        exit
+      }
+    ')
+  fi
+
+  if [ -z "$ssid" ]; then
+    ssid="Unknown"
+  fi
+
+  printf "SSID: %s" "$ssid"
+}
+
 # Speedtestを実施して結果を変数に代入
-ssid=$(system_profiler SPAirPortDataType | awk '/Current Network Information:/{getline; gsub(/^ +|:$/,""); print "SSID: " $0; exit}')
+ssid=$(get_current_ssid)
 output=$(networkQuality)
 uplink=$(echo "$output" | awk -F': ' '/Uplink capacity/{print $2}')
 downlink=$(echo "$output" | awk -F': ' '/Downlink capacity/ {print $2}')
@@ -39,7 +86,7 @@ latest_lines=$(tail -n 10 "$logfile")
 # 各行の内容を必要な情報に分割して表示
 while IFS= read -r line; do
   timestamp=$(echo "$line" | awk -F '\t' '{print $1}')
-  ssid=$(echo "$line" | awk -F '\t' '{print $2}' | awk -F ' ' '{print $2}')
+  ssid=$(echo "$line" | awk -F '\t' '{print $2}' | sed 's/^SSID: //')
   uplink=$(echo "$line" | awk -F '\t' '{print $3}')
   downlink=$(echo "$line" | awk -F '\t' '{print $4}')
   res=$(echo "$line" | awk -F '\t' '{print $5}' | awk -F ' ' '{print $1}')
